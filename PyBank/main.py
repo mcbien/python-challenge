08e818e71006083ec00f480113f416cb304c5cd2
mcbien <mcbien1122@gmail.com>
2020-08-18 22:10:14 -0500

    Saving homework progress

--- PyBank/main.py
+++ PyBank/main.py
@@ -23,23 +23,45 @@ with open(filepath, "r") as csvfile:
         contents.append(row)
         months.append(row[0])
         totals.append(int(row[1]))
-    
-
-# print(contents)
-# print(totals)
-
 
 # Determine the number of items in contents
 count_csvrecords = len(contents)
 
+#Convert Profit / Loss column in contents to integer
+for row in contents:
+    row[1] = int(row[1])
+
 # Determine the Profit / Loss column total
 profitloss = 0
-for i in totals:
-     profitloss = profitloss + i
+greatestincrease = 0
+greatestdecrease = 0
 
-    
+for row in contents:
+    profitloss = profitloss + row[1]
+    if row[1] > greatestincrease:
+        greatestincrease = row[1]
+        greatestincreasemonth = row[0]
+    elif row[1] < greatestdecrease:
+        greatestdecrease = row[1]
+        greatestdecreasemonth = row[0]
+
+# for i in totals:
+#     profitloss = profitloss + i
+#     if i > greatestincrease:
+#         greatestincrease = i
+  
+#         greatestdecrease = i
+
+
+# print(greatestincrease)
+
+# print(greatestdecrease)
+
+
 # Output to terminal
 print("Financial Analysis")
 print("-"*30)
 print(f"Total Months: {count_csvrecords}")
-print(f"Total: {profitloss}")
+print(f"Total: ${profitloss}")
+print(f"Greatest Increase in Profits: {greatestincreasemonth} (${greatestincrease})")
+print(f"Greatest Decrease in Profits: {greatestdecreasemonth} (${greatestdecrease})")
